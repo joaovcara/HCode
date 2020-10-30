6f6c736377b73f59de77f9c5060dda7ef80978bf
João Vitor de Oliveira Cara <62843342+joaovcara@users.noreply.github.com>
2020-10-30 00:06:08 -0300

    DAO-Class

--- PHP/PDO_Orientado/DAO/Config.php
+++ PHP/PDO_Orientado/DAO/Config.php
@@ -3,7 +3,8 @@
     //arquivo de configuração que carrega automaticamente as classes
     spl_autoload_register(function($class_name){
 
-        $filename = $class_name.".php";
+        //A variavel $filename recebe o caminho do arquivo que esta na pasta Class + 'Nome XXXX($class_name)' + extensão(.php)
+        $filename = "Class".DIRECTORY_SEPARATOR.$class_name.".php";
 
         if(file_exists(($filename))){
 
